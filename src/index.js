abb6d5ed76df0a0d3a58f8f8441bec8e205cc733
haniINFP <haniaji1002@gmail.com>
2024-06-20 21:11:45 +0900

    0620 수업

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root')); //하나의 컴포넌트만 쓸 수 있음. 그래서 App밑에 Potato써도 소용 없음
 root.render(
     <App />
+
 );
